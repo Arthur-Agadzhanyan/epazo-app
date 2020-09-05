9d84f2d7ffd6cd3e5a21c91a14c76e4b471f80a7
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2020-09-05 10:02:59 +0300

    'initial'

---
+++ client/build/precache-manifest.b306c9f94f4c3327cc3f0ca86990728f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f809346d1cb53166a200f740e1c695c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fbf408708ba9d0a1cf5",
+    "url": "/static/css/main.69a0d59f.chunk.css"
+  },
+  {
+    "revision": "f6230b4104db4f041725",
+    "url": "/static/js/2.8ebdec62.chunk.js"
+  },
+  {
+    "revision": "0ca0de0194ca5a13d45d612cb80385a9",
+    "url": "/static/js/2.8ebdec62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fbf408708ba9d0a1cf5",
+    "url": "/static/js/main.7cf21ab3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "eecd7f5323c829800100ee3206239dea",
+    "url": "/static/media/1.eecd7f53.jpg"
+  }
+]);
